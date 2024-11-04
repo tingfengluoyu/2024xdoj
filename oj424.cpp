4b8a0abf7d1ef4041e8fe4ecc4560dc17a105208
听风落雨 <1462368052@qq.com>
2024-11-04 20:27:10 +0800

    更新 oj424.cpp

--- oj424.cpp
+++ oj424.cpp
@@ -44,9 +44,17 @@ int main()
     }
 
     if(yue == 1 && ri == 31)
+    int arr[4] = {4,6,9,11};
+    int brr[7] = {1,3,5,7,8,10,12};
+    for(int i = 0; i < 4; i++)
     {
         yue += 1;
         ri = 1;
+        if(yue == arr[i] && ri == 31)
+        {
+            ri = 1;
+            yue += 1;
+        }
     }
     if(yue == 3 && ri == 31)
      {
@@ -57,10 +65,19 @@ int main()
      {
         yue += 1;
         ri = 1;
+    for(int i = 0; i < 7; i++)
+    {
+        if(yue == brr[i] && ri == 32)
+        {
+            ri = 1;
+            yue += 1;
+        }
     }
     if(yue == 7 && ri == 31)
      {
         yue += 1;
+    if(yue == 2 && runnian(nian) && ri == 30)//是闰年且为2月29日
+    {
         ri = 1;
     }
     if(yue == 8 && ri == 31)
@@ -80,14 +97,18 @@ int main()
         ri = 1;
     }
     if(yue == 2 && runnian(nian) && ri == 29)
+    else if(yue == 2 && !runnian(nian) && ri == 29)//不是闰年且为2月28日
     {
-        yue += 1;
         ri = 1;
+        yue += 1;
     }
     if(yue == 2 && !runnian(nian) && ri == 28)
+    if(yue == 13)
     {
         yue += 1;
         ri = 1;
+        yue = 1;
+        nian += 1;
     }
 
     printf("%d\n%d\n%d\n%d\n%d\n%d\n",nian,yue,ri,shi,fen,miao);
