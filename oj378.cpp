c58e9dabc4357e1b6a161ed619fc64a4a1c1eb87
听风落雨 <1462368052@qq.com>
2024-11-05 18:27:26 +0800

    删除 oj378.cpp

--- oj378.cpp
+++ oj378.cpp
@@ -19,26 +19,3 @@ int main(){
 	}
 	return 0;
 }
-int main()
-{
-    long long n = 0;
-    scanf("%lld",&n);
-    if(n % 2 == 1)
-    {
-        printf("-1\n");
-        return 0;
-    }
-    else
-    {
-        for(int i = 100; i > 0; i--)
-        {
-            if(n >= pow(2,i))
-            {
-                printf("%d ",(int)pow(2,i));
-                n -= pow(2,i);
-            }
-        }
-        printf("\b");
-    }
-    return 0;
-}
