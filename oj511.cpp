524639b90bf9d126d1d5c439df7b554416b83ee9
听风落雨 <1462368052@qq.com>
2024-11-06 12:24:48 +0800

    更新 oj511.cpp

--- oj511.cpp
+++ oj511.cpp
@@ -2,31 +2,20 @@
 #include<math.h>
 int main()
 {
-    long long high = 0;//高度
-    long long n = 0;//落地次数
-    scanf("%lld %lld",&high,&n);
+    double high = 0;      //高度
+    int n = 0;         //落地次数
+    scanf("%lf %d",&high,&n);
 
-    long long x = 0;//x表示距离
-    double gao = 0;//gao表示第n次落地时的反弹高度
-    for(long long i = 0;i < n;i++)
+    double x = 0;           //x表示距离
+    double gao = 0;      //gao表示第n次落地时的反弹高度
+    
+    for(int i = 0; i < n - 1; i++)
     {
-        if(n == 1)
-        {
-            x = high;
-        }
-        else
-        {
-            x += 2 * high / pow(2,i);
-        }
-        if (i == n - 1)
-        {
-                gao = (double)high / pow(2,n);
-        }
+        gao = high / pow(2,i + 1);
+        x += 2.0 * gao;
     }
-    if(n > 1)
-    {
-    x -= high;
-    }
-    printf("%.1f %.1f",(double)x,gao);
+    gao /= 2.0;
+    x += high;
+    printf("%.1f %.1f",x,gao);
     return 0;
 }
