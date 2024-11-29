bac6a563fb66f7dabe28e100023b96c8d33fc786
听风落雨 <1462368052@qq.com>
2024-11-29 15:27:42 +0800

    Update oj559.cpp

--- oj559.cpp
+++ oj559.cpp
@@ -1,29 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
-#include<math.h>
+#include<stdlib.h>
+
 int main()
 {
     int n = 0;
-    scanf("%d",&n);
-    for(int i = 0;i < n;i++)
-    {
-        int a = 0;
-        scanf("%d",&a);
-        double weishu = 0;
-        int b = a;
-        while(b > 0)
-        {
-            weishu++;
-            b /= 10.0;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        int a = arr[i];
+        int len = 0;
+
+        while (a > 0) {
+            len++;
+            a /= 10;
         }
-        double c = (a * a) % (int)pow(10.0,weishu);
-        if(a == c)
-        {
-            printf("%d\n",a);
+
+        int square = arr[i] * arr[i];
+        int weishu = 1;
+        for (int j = 0; j < len; j++) {
+            weishu = weishu * 10;
         }
-        else
-        {
+
+        int lastDigits = square % weishu; // 获取平方数的最后len位
+
+        if (lastDigits == arr[i]) {
+            printf("%d\n", arr[i]);
+        }
+        else {
             printf("No\n");
         }
     }
+
+    free(arr);
     return 0;
 }
