b5d79513ef87f7a7ca98c30301293e5120598f2f
听风落雨 <1462368052@qq.com>
2024-12-01 09:20:08 +0800

    Update oj683.cpp

--- oj683.cpp
+++ oj683.cpp
@@ -1,26 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
+#include<stdlib.h>
+
 int main()
 {
-    int n = 0;
-    scanf("%d",&n);
-    int arr[n] = {0};
-    int brr[n] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    
-    for (int i = 1; i < n - 1; i++)
-    {
-        brr[i] = (arr[i - 1] + arr[i] + arr[i + 1]) / 3;
-    }
+	int n = 0;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
 
-    brr[0] = brr[1];
-    brr[n - 1] = brr[n - 2];
-    
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ",brr[i]);
-    }
-    return 0;
+	int* brr = (int*)malloc(n * sizeof(int));
+
+	for (int i = 1; i < n - 1; i++) {
+		brr[i] = (arr[i - 1] + arr[i] + arr[i + 1]) / 3;
+	}
+
+	brr[0] = brr[1];
+	brr[n - 1] = brr[n - 2];
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", brr[i]);
+	}
+
+	free(arr);
+	free(brr);
+	return 0;
 }
