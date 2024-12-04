fb70b46e6f2505212d224612e0ca955dbac255bd
听风落雨 <1462368052@qq.com>
2024-12-04 20:16:55 +0800

    Update oj686.cpp

--- oj686.cpp
+++ oj686.cpp
@@ -1,27 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
+
 int main()
 {
-    int n = 0;
-    scanf("%d",&n);
-    int arr[n] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d",&arr[i]);
-    }
+	int n = 0;
+	int arr[20] = { 0 };
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
 
-    int i = 0;
-    int sum = 0;
+	int sum = 0;
+	int i = 0;
 
-    do
-    {
-        sum += arr[i] % 10;
-        arr[i] /= 10;
-        if(arr[i] == 0)
-        {
-            i++;
-        }
-    }
-    while (i < n);
-    
-    printf("%d",sum % 10);
+	while (i < n) {
+		sum += arr[i] % 10;
+		arr[i] /= 10;
+		if (arr[i] == 0) {
+			i++;
+		}
+	}
+
+	printf("%d", sum % 10);
+
+	return 0;
 }
