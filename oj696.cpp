b3336dc69b04f7aa45c4ae1f7f10015604e8ea4a
听风落雨 <1462368052@qq.com>
2024-12-06 09:04:42 +0800

    Update oj696.cpp

--- oj696.cpp
+++ oj696.cpp
@@ -1,52 +1,44 @@
 #include<stdio.h>
-int main ()
+
+int main()
 {
-    int n = 0;
-    scanf("%d",&n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d",&arr[i]);
-    }
+	int n = 0;
+	scanf("%d", &n);
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
 
-    int max = 0;
-    int min = 0;
+	int max = 0;
+	int min = 0;
 
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > arr[max])
-        {
-            max = i;
-        }
-    }
+	for (int i = 0; i < n; i++) {
+		if (arr[max] < arr[i]) {
+			max = i;
+		}
+	}
 
-    if (max != n - 1)
-    {
-        int t = arr[n - 1];
-        arr[n - 1] = arr[max];
-        arr[max] = t;
-    }
+	if (max != n - 1) {
+		int t = arr[max];
+		arr[max] = arr[n - 1];
+		arr[n - 1] = t;
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < arr[min])
-        {
-            min = i;
-        }
-    }
-    
-    if (min != 0)
-    {
-        int a = arr[0];
-        arr[0] = arr[min];
-        arr[min] = a;
-    }
+	for (int i = 0; i < n; i++) {
+		if (arr[min] > arr[i]) {
+			min = i;
+		}
+	}
+	
+	if (min != 0) {
+		int x = arr[min];
+		arr[min] = arr[0];
+		arr[0] = x;
+	}
 
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-         printf("%d ",arr[i]);
-    }
-
-    return 0;
+	return 0;
 }
