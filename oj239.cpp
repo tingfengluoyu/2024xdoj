a57a6e2997f45f72028723f9922eb20620d20800
听风落雨 <1462368052@qq.com>
2024-12-16 21:39:01 +0800

    Update oj239.cpp

--- oj239.cpp
+++ oj239.cpp
@@ -1,25 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
+
 int main()
 {
-    int n = 0;
-    scanf("%d",&n);
-    
-    int arr[n][n];
-    for (int i = 0; i < n; i++)
-    {
-        arr[i][0] = 1;
-        arr[i][i] = 1;
+	int n = 0;
+	scanf("%d", &n);
+	int arr[10][10] = { 0 };
 
-        for (int k = 1; k < i; k++)
-        {
-            arr[i][k] = arr[i - 1][k - 1] + arr[i - 1][k];
-        }
-    
-        for (int k = 0; k <= i; k++)
-        {
-            printf("%d ",arr[i][k]);
-        }
-        printf("\n");
-    }
-    return 0;
+	arr[0][0] = arr[1][0] = arr[1][1] = 1;
+
+	for (int i = 2; i < 10; i++) {
+		arr[i][0] = arr[i][i] = 1;
+		for (int j = 1; j < i; j++) {
+			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
