f31ec388d1b633c966ac852ba20c6e6fd9f2d713
听风落雨 <1462368052@qq.com>
2024-12-20 15:02:40 +0800

    Create oj39.cpp

---
+++ oj39.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main()
+{
+    int m = 0;
+    int n = 0;
+    scanf("%d %d", &m, &n);
+
+    int arr[100][100];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int found = 0;
+
+    for (int i = 0; i < m && !found; i++) {
+        int min = arr[i][0];
+        int x = 0;
+        for (int j = 1; j < n; j++) {
+            if (min > arr[i][j]) {
+                min = arr[i][j];
+                x = j;
+            }
+        }
+
+        int max = arr[0][x];
+        int y = x;
+        for (int t = 1; t < m; t++) {
+            if (max < arr[t][x]) {
+                max = arr[t][x];
+                y = x;
+            }
+        }
+
+        if (min == max) {
+            found = 1;
+            printf("%d %d %d\n", i, x, max);
+        }
+    }
+
+    if (found == 0) {
+        printf("no\n");
+    }
+
+    return 0;
+}
