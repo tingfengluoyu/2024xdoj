003644481b2178fb6b078bc129b1d2d1f5e8c371
听风落雨 <1462368052@qq.com>
2024-12-28 16:00:08 +0800

    Update oj691.cpp

--- oj691.cpp
+++ oj691.cpp
@@ -1,45 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
 
-// 交换两个整数的函数
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+#include<stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n);
-    int* arr = (int*)malloc(n * sizeof(int));
-    if (arr == NULL) {
-        printf("内存分配失败\n");
-        return 1;
-    }
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+int main()
+{
+	int n = 0;
+	//1<=n<=30
+	scanf("%d", &n);
+	int arr[30];
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
 
-    int left = 0;
-    int right = n - 1;
-    if (n % 2 == 1) {
-        // 奇数个元素时，中间元素不动，调整左右边界
-        left = (n + 1) / 2 - 1;
-        right = (n + 1) / 2;
-    }
+	if (n % 2 == 1) {
+		int mid = (n - 1) / 2;
+		for (int i = 0; i < mid; i++) {
+			int t = arr[i];
+			arr[i] = arr[mid + i + 1];
+			arr[mid + i + 1] = t;
+		}
+	}
+	else {
+		int mid = n / 2;
+		for (int i = 0; i < mid; i++) {
+			int t = arr[i];
+			arr[i] = arr[mid + i];
+			arr[mid + i] = t;
+		}
+	}
 
-    // 通过双指针法进行折半交换
-    while (left >= 0 && right < n) {
-        swap(&arr[left], &arr[right]);
-        left--;
-        right++;
-    }
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    free(arr);
-    return 0;
+	return 0;
 }
